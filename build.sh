a6e980cf5f6d25ca025cecf093625b23d8501492
Tong Zhang <t.zhang2@partner.samsung.com>
2018-06-13 14:23:15 -0700

    cmake compiler

--- build.sh
+++ build.sh
@@ -1,9 +1,15 @@
 #!/bin/bash
+
+#specify non default compiler here
+#-DCMAKE_C_COMPILER= 
+#-DCMAKE_CXX_COMPILER= 
+
 mkdir build
 pushd build
 cmake ../ \
     -DLLVM_DIR=/opt/toolchain/llvm-git \
     -DLLVM_ROOT=/opt/toolchain/llvm-git \
-    -DCMAKE_BUILD_TYPE=Debug
+    -DCMAKE_BUILD_TYPE=Debug \
+
 make -j
 popd
