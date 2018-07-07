e2d5e21b43ad30f9748e0a906ab313a6e32be1d1
Tong Zhang <t.zhang2@partner.samsung.com>
2018-07-06 18:11:28 -0700

    fix for llvm-6

--- capchk/gating_function.cpp
+++ capchk/gating_function.cpp
@@ -7,6 +7,7 @@
 #include "color.h"
 
 #include "llvm/IR/CallSite.h"
+#include "llvm/Support/raw_ostream.h"
 
 #include "utility.h"
 ////////////////////////////////////////////////////////////////////////////////
